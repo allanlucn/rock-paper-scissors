ff6321c3db4c28bb7b930efc8e4d4b5b7e9a1beb
Allan <172357148+allanlucn@users.noreply.github.com>
2025-10-14 18:18:27 -0300

    terminei o exercício e já é executável pelo console

---
+++ scripts.js
@@ -0,0 +1,69 @@
+function getComputerChoice(){
+    const randomNumber = Math.random();
+
+    if (randomNumber <= 0.33){
+        return "Pedra";
+    }
+    else if (randomNumber <= 0.66){
+        return "Papel";
+    }
+    else{
+        return "Tesoura";
+    }
+}
+
+function getHumanChoice(){
+    let userChoice = prompt("Qual a sua escolha? (Pedra, Papel, ou Tesoura)?")
+    return userChoice;
+}
+
+function playGame(){
+    let humanScore =0;
+    let computerScore=0;
+    let humanSelection;
+    let computerSelection;
+ 
+ 
+    function playRound(HumanChoice,ComputerChoice){
+    const human= HumanChoice.toLowerCase();
+    const computer= ComputerChoice.toLowerCase();
+    if (
+        human === computer){
+            console.log(`Empate! Os dois escolheram ${computer}`);
+    }
+    else if(
+        (human === "pedra" && computer === "tesoura") ||
+        (human === "papel" && computer === "pedra") ||
+        (human === "tesoura" && computer ==="papel")){
+            humanScore++;
+            console.log(`Você venceu! ${human} ganha de ${computer}.`)
+            }
+    else{
+        computerScore++;
+        console.log(`Você perdeu! ${computer} ganha de ${human}.`)
+    }
+
+}
+
+    for (let i = 1; i <=5; i++){
+        console.log (`Rodada ${i}`)
+        humanSelection= getHumanChoice();
+        computerSelection = getComputerChoice();
+
+        playRound(humanSelection,computerSelection)
+    }
+
+    console.log("Terminou")
+    console.log(`Placar final: Humano ${humanScore} x ${computerScore} Computador.`);
+    if (humanScore > computerScore){
+        console.log("Você venceu!")
+    }
+    else if(computerScore > humanScore){
+        console.log("Você perdeu!")
+    }
+    else{
+        console.log("O jogo terminou em Empate!")
+    }
+}
+
+playGame();
